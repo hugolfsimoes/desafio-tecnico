64bfde529a26506f6a706db1a02f9d36bc9efee9
hugolfsimoes <hugolfsimoes@gmail.com>
2021-10-29 18:30:24 -0300

    instalado o react router dom e dado inicio na criacao da pagina de login

---
+++ front-end/src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+export default function index() {
+  return (
+    <form>
+      <label htmlFor="">E-mail</label>
+      <input type="email" id="input-email" />
+
+      <label htmlFor="">Password</label>
+      <input type="password" input-password />
+
+    </form>
+  )
+}
